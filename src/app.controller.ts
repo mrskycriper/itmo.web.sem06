2a4d8213d0717cd2b1b797b3012f956bbe674405
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2022-03-09 01:01:09 +0300

    first working version completed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,46 @@
-import { Get, Controller, Render } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Get,
+  Controller,
+  Render,
+  Post,
+  Req,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 
+import { Response, Request } from 'express';
+import { LoggingInterceptor } from './logging.interceptor';
+
+@UseInterceptors(LoggingInterceptor)
 @Controller()
 export class AppController {
-  @Get()
-  @Render('index')
-  root() {
-    return { message: 'AMOGUS!' };
+  async root() {
+    await new Promise((resolve) =>
+      setTimeout(resolve, 100 + Math.floor(Math.random())),
+    );
+  }
+
+  @Get('/')
+  @Render('main')
+  root1() {
+    return { title: 'Главная - OpenForum', authorised: true};
+  }
+
+  @Get('/topics')
+  @Render('topics')
+  root2() {
+    return { title: 'Разделы - OpenForum' };
+  }
+
+  @Get('/chats')
+  @Render('chats')
+  root3() {
+    return { title: 'Чаты - OpenForum' };
+  }
+
+  @Post('login')
+  loginUser(@Res() response: Response, @Req() request: Request) {
+    response.cookie('user', request.body.username);
+    response.end();
   }
 }
